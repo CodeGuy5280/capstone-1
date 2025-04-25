157e83cc25f67420b47270e72207b6505d7bf730
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-25 16:14:45 -0500

    restructuring the files

--- AccountLedger/src/main/java/com/pluralsight/Main.java
+++ AccountLedger/src/main/java/com/pluralsight/Main.java
@@ -1,25 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
 
-    //use local date.parse
-    //credit -> in | debit -> out
-    //create separate classes for in and out
-    //display transactions
-    //load -> add -> save (persistence of data)
-    //newest first = loop backward (i--) .length--
-    //be weary of using substrings for searching to allow partial search terms
-    //look into personalizing the app for coloring of transactions, etc.
     public static void main(String[] args) {
         //using main class to run program -> all function calling will be here to initiate that portion of the App depending on user input.
 
+        //use local date.parse
+        //credit -> in | debit -> out
+        //create separate classes for in and out
+        //display transactions
+        //load -> add -> save (persistence of data)
+        //newest first = loop backward (i--) .length--
+        //be weary of using substrings for searching to allow partial search terms
+        //look into personalizing the app for coloring of transactions, etc.
 
         //Integrate the homescreen into the Main Class, not use its own Class file.
 
 
-    }
 
-    public static class HomeScreen {
+
+    public class HomeScreen {
+        Scanner scanner = new Scanner(System.in);
 
         //each choice will navigate to a different screen of the program
         //options to direct to next app screen
@@ -33,21 +36,31 @@ public class Main {
         String Exit;
 
 
-        public void setDeposit(){
+        public void appDeposit(){
             System.out.println("What amount would you like to deposit?: ");
+            deposit = scanner.nextDouble();
+            System.out.println(deposit);
         }
 
 
-        public void setPayment(){
+        public void appPayment(){
             System.out.println("What amount would you like to pay?: ");
+            payment = scanner.nextDouble();
         }
 
 
+        boolean appExit = true;
+        public void appExit(){
+            System.out.println(("Exit Application? (Y/N): "));
+            if(appExit){
+                System.out.println("Exiting Application.");
 
-        public void setExit(){
-            System.out.println("Confirming will exit the application. (Y/N): ");
+               scanner.close();
+
+            }
         }
 
     }
 
+}
 }
