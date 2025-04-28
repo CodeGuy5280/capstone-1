6ffac22757b5e9d5d10edac25d4099ad63108a9a
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-28 13:28:35 -0500

    added a nested switch statement for ledger. Still need to display all individual options

--- AccountLedger/src/main/java/com/pluralsight/Main.java
+++ AccountLedger/src/main/java/com/pluralsight/Main.java
@@ -32,32 +32,39 @@ public class Main {
                     break;
 
                 case 3:
-                    int options = 5;
-                    System.out.println("Ledger options are " + options);
-                    switch (options){
-                        case 1:
-                            System.out.println("1: Display all entries");
-                            break;
-                        case 2:
-                            System.out.println("2: Display deposits");
-                            break;
-                        case 3:
-                            System.out.println("3: Display payments");
-                            break;
-                        case 4:
-                            System.out.println("4: Display reports");
-                            break;
-                        case 5:
-                            System.out.println("Exit to homescreen");
-                        default:
-                            System.out.println("");
+                    boolean viewLedger = true;
+                    while (viewLedger) {
+                        System.out.println("Ledger options: ");
+                        System.out.println("1: Display all entries");
+                        System.out.println("2: Display deposits");
+                        System.out.println("3: Display payments");
+                        System.out.println("4: Display reports");
+                        System.out.println("5: Exit to homescreen");
+                        int options = scanner.nextInt();
+                        switch (options) {
+                            case 1:
+                                System.out.println("1: Display all entries");
+                                break;
+                            case 2:
+                                System.out.println("2: Display deposits");
+                                break;
+                            case 3:
+                                System.out.println("3: Display payments");
+                                break;
+                            case 4:
+                                System.out.println("4: Display reports");
+                                break;
+                            case 5:
+                                System.out.println("Exit to homescreen");
+                                viewLedger = false;//false to close loop back to homescreen
+                            default:
+                                System.out.println("Returning to homescreen...\n");//new line added for separation in CLI
+                        }
                     }
-
-
                     break;
 
                 case 4:
-                    System.out.println("Exiting the app.");
+                    System.out.println("Exiting the app...");
 
                     running = false; //setting to false closes the loop
                     break;
